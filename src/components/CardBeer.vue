333bcca205354934a645a844d3941d783033b825
Beleks <53120159+Beleks@users.noreply.github.com>
2020-10-17 23:00:57 +0700

    add svg

--- src/components/CardBeer.vue
+++ src/components/CardBeer.vue
@@ -1,14 +1,49 @@
 <template>
   <div>
-    <div></div>
-    <div></div>
+    <div class="head">
+      <div class="left">
+        <div class="number">1</div>
+        <div class="name">Buzz</div>
+      </div>
+      <div class="right">
+        <div class="edit">
+          <Edit></Edit>
+        </div>
+        <div class="delete">
+          <Delete></Delete>
+        </div>
+      </div>
+    </div>
+    <div class="body">
+      <div class="photo">
+        <img src="" alt="">
+      </div>
+      <div class="description">
+        <div class="des">
+          <div class="title">Description</div>
+          <div class="text">Какой-то текст</div>
+        </div>
+        <div class="tips">
+          <div class="title">Brewers tips</div>
+          <div class="text">Какой-то текст</div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
+import Beer from "./svg/Beer";
+import Delete from "./svg/Delete";
+import Edit from "./svg/Edit";
 
-}
+export default {
+  components: {
+    Beer,
+    Delete,
+    Edit,
+  },
+};
 </script>
 
 <style lang="sass" scoped>
